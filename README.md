33690a8396786de86324be718e9d794512721e0a
xymy <thyfan@163.com>
2019-11-30 16:21:55 +0800

    Add config

---
+++ README.md
@@ -0,0 +1,23 @@
+# hellopy
+
+*Hello, Python!*
+
+## Installation
+
+### 1. Install dependencies
+
+    pip3 install -U setuptools wheel
+
+### 2. Download the source code
+
+    git clone https://github.com/xymy/hellopy.git
+
+### 3. Build wheel
+
+    cd hellopy
+    python3 setup.py build bdist_wheel
+
+### 4. Install wheel
+
+    cd dist
+    pip3 install hellopy-1.0-py3-none-any.whl
---
+++ setup.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+import hellopy
+
+readme = Path(__file__).with_name('README.md').read_text()
+
+classifiers = [
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+]
+
+setup(
+    name='hellopy',
+    version=hellopy.__version__,
+    description='Hello, Python!',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='xymy',
+    author_email='thyfan@163.com',
+    classifiers=classifiers,
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'hellopy = hellopy.hellopy:main'
+        ]
+    },
+    python_requires='>=3.6'
+)
