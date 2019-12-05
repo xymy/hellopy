e0e8f98c625bcf968ca89475ec1c507629c71d76
xymy <thyfan@163.com>
2019-12-05 11:36:27 +0800

    Add some functions

--- hellopy/hellopy.py
+++ hellopy/hellopy.py
@@ -6,13 +6,21 @@ from click.termui import _ansi_colors
 colors = list(_ansi_colors)
 
 
-def main():
-    message = 'Hello, Python!'
+def plain(message):
+    click.echo(message)
+
+
+def colorful(message):
     for c in message:
         fg = random.choice(colors)
         bg = random.choice(colors)
         click.secho(c, nl=False, fg=fg, bg=bg)
     click.echo()
+
+
+def main():
+    message = 'Hello, Python!'
+    colorful(message)
 
 
 if __name__ == '__main__':
